41f0bd21f0882ad9e4c24751d583d57e25e0de93
Outlandstalker <volkerhof@mx.de>
2013-08-12 17:35:10 +0200

    Replaced angularjs-i18n with angular-translate

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,15 +1,14 @@
 'use strict';
 
-angular.module('NGi18nTestApp', ['I18nAngular'])
-    .config(function ($routeProvider) {
+var app = angular.module('NGi18nTestApp', ['pascalprecht.translate']);
+
+app.config(function ($routeProvider) {
         $routeProvider
             .when('/home', {
-                templateUrl: 'views/home.html',
-                controller: 'I18nCtrl'
+                templateUrl: 'views/home.html'
             })
             .when('/format', {
-                templateUrl: 'views/format.html',
-                controller: 'I18nCtrl'
+                templateUrl: 'views/format.html'
             })
             .otherwise({
                 redirectTo: '/home'
